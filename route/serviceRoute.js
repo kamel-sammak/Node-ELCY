e765cde0ba87d4d48ee52906338367f3b9d3fb35
kamel-sammak <kamel.sammak@gmail.com>
2024-01-07 01:24:24 +0300

    first commit

--- route/serviceRoute.js
+++ route/serviceRoute.js
@@ -2,55 +2,66 @@ const express = require('express');
 const router = express.Router();
 
 const Service = require("../models/serviceModels");
-
-
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const Image = require('../models/imageModels');
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = 'C:\\Users\\kamel\\Desktop\\New folder';
-        cb(null, uploadPath);
+// Storage configuration for multer
+const Storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+    storage: Storage
+}).single('image');
 
-router.post("/addService", upload.single('image'), async (request, response) => {
+
+router.post("/addService", async (request, response) => {
     try {
-        const { name } = request.body;
-
-        // Check if a service with the same name already exists
-        const existingService = await Service.findOne({ name });
-
-        if (existingService) {
-            return response.status(400).json({ message: "Service with the same name already exists" });
-        }
-
-        let imageUrl = null;
-
-        if (request.file) {
-            // If an image was uploaded, use the uploaded image
-            imageUrl = request.file.path;
-        } else if (request.body.imagePath) {
-            // If no image was uploaded but imagePath is provided in the request body, use the provided imagePath
-            imageUrl = request.body.imagePath;
-
-            // You may want to check if the file at the provided path exists before saving it in the database
-            if (!fs.existsSync(imageUrl)) {
-                return response.status(400).json({ message: "Image file does not exist" });
+        upload(request, response, async (err) => {
+            if (err) {
+                console.log(err);
+                return response.status(500).json({ status: 500, message: 'Error uploading image', error: err.message });
             }
-        }
 
-        // If no existing service, create a new one
-        const service = await Service.create({ name, imageUrl });
+            try {
+                const existingService = await Service.findOne({ name: request.body.name });
 
-        response.status(200).json(service);
+                if (existingService) {
+                    // Service with the same name already exists
+                    return response.status(400).json({ status: 400, message: 'Service with the same name already exists' });
+                }
+
+                // Create a new image document
+                const newImage = new Image({
+                    name: request.body.name,
+                    image: {
+                        data: request.file.filename,
+                        contentType: request.file.mimetype,
+                    },
+                });
+
+                // Save the image document
+                await newImage.save();
+
+                // Create a new service document with the image details
+                const service = await Service.create({
+                    id: request.body.id,
+                    name: request.body.name,
+                    imageUrl: 'http://localhost:3000/uploads/' + newImage.name + '.' + newImage.image.contentType.split('/')[1], // Set the image URL in the service document
+                    image: newImage,
+                });
+                response.status(200).json(service);
+
+            } catch (saveError) {
+                console.error(saveError);
+                response.status(500).json({ status: 500, message: 'Error saving image data', error: saveError.message });
+            }
+        });
+
     } catch (error) {
         response.status(500).json({ message: error.message });
     }
@@ -58,12 +69,7 @@ router.post("/addService", upload.single('image'), async (request, response) => 
 
 
 
-
-
-
-
-
-router.post("/addService1", async (request, response) => {
+router.post("/addServiceOLD", async (request, response) => {
     try {
         const { name } = request.body;
 
@@ -85,13 +91,9 @@ router.post("/addService1", async (request, response) => {
 
 
 
-
-
-
-
 router.get("/getAllServices", async (request, response) => {
     try {
-        const services = await Service.find({}).select(' name '); // Exclude the _id field ('name -_id')
+        const services = await Service.find({}); // Exclude the _id field ('name -_id')
         if (services.length > 0) {
             response.status(200).json({ services });
         } else {
